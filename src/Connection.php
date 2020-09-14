c4a681847ff58428ea26ce597fa23d7c84f5c016
Thiago Cordeiro <thiagoguetten@gmail.com>
2020-09-14 19:50:57 -0300

    Added pdo params on connection

--- src/Connection.php
+++ src/Connection.php
@@ -11,9 +11,12 @@ class Connection
 {
     protected PDO $pdo;
 
-    public function __construct(string $dsn)
+    /**
+     * @param mixed[] $options
+     */
+    public function __construct(string $dsn, ?string $user = null, ?string $password = null, array $options = [])
     {
-        $this->pdo = new PDO($dsn);
+        $this->pdo = new PDO($dsn, $user, $password, $options);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     }
